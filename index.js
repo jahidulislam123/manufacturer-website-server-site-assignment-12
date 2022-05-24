43d31074d13d113b9f4e1a005e7620eb81638bf8
unknown <islam15-3879@diu.edu.bd>
2022-05-25 02:46:06 +0600

    changed

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req,res,next){
   console.log('abc')
   const authHeader =req.headers.authorization;
+ 
   if(!authHeader){
     return res.status(401).send({message:'Unauthorized access'})
   }
   const token  =authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded) {
    if(error){
      return res.status(403).send({message :'forbidden access'})
    }
