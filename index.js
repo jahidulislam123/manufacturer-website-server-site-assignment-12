7d6ae5f4a3fa6caad77bb94e508f47ddab0b778a
unknown <islam15-3879@diu.edu.bd>
2022-05-25 11:50:44 +0600

    make-admin-finish

--- index.js
+++ index.js
@@ -59,7 +59,39 @@ function verifyJWT(req,res,next){
           const token = jwt.sign({email : email},process.env.ACCESS_TOKEN_SECRET, {expiresIn :'1h'})
           res.send({result, token});
 
+        });
+
+
+
+        app.get('/admin/:email',async(req,res)=>{
+          const email =req.params.email;
+          const user =await userCollection.findOne({email:email})
+          const isAdmin =user.role === 'admin';
+          res.send({admin :isAdmin})
         })
+
+
+        app.put('/user/admin/:email',verifyJWT, async(req,res)=>{
+          const email =req.params.email;
+         const requester = req.decoded.email;
+         const requesterAccount =await userCollection.findOne({email:requester});
+         if(requesterAccount.role === 'admin'){
+          const filter = {email:email};
+         
+          const updateDoc = {
+            $set: {role:'admin'},
+          };
+          const result = await userCollection.updateOne(filter,updateDoc);
+          res.send(result);
+         }
+         else{
+           res.status(403).send({message:'forbidden'})
+         }
+        
+
+        })
+
+
 
         app.post('/booking',async(req,res)=>{
             const booking =req.body;
@@ -113,7 +145,7 @@ function verifyJWT(req,res,next){
           
         })
 
-        app.get('/user', async(req,res)=>{
+        app.get('/user',verifyJWT, async(req,res)=>{
           const users =await userCollection.find().toArray();
           res.send(users);
         })
