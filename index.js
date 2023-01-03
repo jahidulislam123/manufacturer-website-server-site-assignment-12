01bdeb826275c11acd546ac77426292d9c1b05e4
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-01-03 16:53:00 +0600

    added-new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { send } = require('express/lib/response');
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 4000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
